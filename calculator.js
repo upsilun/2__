001473c43588b337b7d228c125e51d270c302ae3
Yazeed Alahmad <72664813+upsilun@users.noreply.github.com>
2024-01-02 05:57:46 -0800

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,36 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const handleInput = () => {
+  rl.question('~ ', (input) => {
+    if (['exit', 'quit', 'close', 'end'].includes(input.toLowerCase())) {
+      rl.close();
+    }
+    else {
+      try {
+        const result = evalInput(input);
+        console.log(`${result}`);
+        handleInput();
+      } catch (error) {
+        console.error('Error:', error.message);
+        handleInput();
+      }
+    }
+  });
+};
+
+const evalInput = (expression) => {
+  return eval(expression.replace(/sin/g, 'Math.sin')
+                        .replace(/cos/g, 'Math.cos')
+                        .replace(/cot/g, '1 / Math.tan')
+                        .replace(/csc/g, '1 / Math.sin')
+                        .replace(/sec/g, '1 / Math.cos')
+                        .replace(/pi/g, 'Math.PI')
+                        .replace(/\^/g, '**'));
+};
+
+handleInput();
